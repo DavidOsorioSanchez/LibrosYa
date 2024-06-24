7e324a498ff5871d29df50263a87fbadaefe0dfe
-- <-->
2024-06-24 08:17:07 -0500

    cambiando

--- src/main/java/com/riwi/LibrosYa/config/swagger/SwaggerConfig.java
+++ src/main/java/com/riwi/LibrosYa/config/swagger/SwaggerConfig.java
@@ -18,7 +18,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("tu.paquete.base"))
+                .apis(RequestHandlerSelectors.basePackage("com.riwi.LibrosYa"))
                 .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
